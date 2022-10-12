6a548ddcfa876b8a3198423188c2c002b01bf90d
yuvihere23 <114753071+yuvihere23@users.noreply.github.com>
2022-10-12 03:04:12 -0700

    Added solution to distict permution problem in cpp

---
+++ Codechef/Distinct_permutation.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define all(A) A.begin,A.end()
+#define ll long long
+using namespace std;
+
+int main() {
+ ios_base::sync_with_stdio(false);
+ cin.tie(0);
+ int T;
+cin>>T;
+while(T--){
+ int N;
+ cin>>N;
+ for(int i=0;i<N;i++){
+ cout<<N-i<<" ";
+ }
+ cout<<endl;
+ }
+return 0;
+}
