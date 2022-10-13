e2e00d2bf8c4792f7213d941e1aebc0004d29cda
Aman Kushwaha <amankushwaha008@gmail.com>
2022-10-13 18:15:39 +0530

    Added Longest Substring Without Repeating Characters Problem

--- Leetcode/Longest_substring_without_repeating_characters.cpp
+++ Leetcode/Longest_substring_without_repeating_characters.cpp
@@ -1,22 +1,56 @@
-// problem link
-// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+//Username: amankushwaha008
+//AIM : to add Longest Substring Without Repeating Characters`s solution to your repository
+//date: 11 Oct 2022
+
+
+
+// Longest Substring Without Repeating Characters
+
+/*
+Given a string s, find the length of the longest substring without repeating characters.
+
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+*/
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        map<char , int> mp;
-        int l = 0 , r = 0;
-        int ans = 0;
-        for(r = 0 ; r < size(s) ; r++)
+        int res = 0, n = s.size(), curr=0, arr[256]={0};
+        
+        for(int i=0; i<n; i++)
         {
-            mp[s[r]]++;
-            
-            while(mp[s[r]] > 1)
+            while(arr[s[i]]!=0)
             {
-                mp[s[l++]]--;
+                arr[s[i-curr]]--;
+                curr--;
             }
-            ans = max(ans , r-l+1);
+            if(arr[s[i]]==0)
+            {
+                arr[s[i]]=1;
+                curr++;
+            }
+            res = max(res, curr);
         }
-        return ans;
+        
+        return res;
     }
 };
