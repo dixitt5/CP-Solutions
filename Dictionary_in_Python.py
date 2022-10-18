7ea6b1606706b05c659fb58ae464613587a15255
Ankit Agarwal <114647969+ankitaga27@users.noreply.github.com>
2022-10-18 16:45:38 +0530

    Create Dictionary_in_Python.py
    Please add my file with label hacktoberfest-2022

---
+++ Dictionary_in_Python.py
@@ -0,0 +1,28 @@
+
+Creating an empty Dictionary
+Dict = {}
+print("Empty Dictionary: ")
+print(Dict)
+
+Adding elements one at a time
+Dict[0] = 'hackoctberfest'
+Dict[2] = 'For'
+Dict[3] = 1
+print("\nDictionary after adding 3 elements: ")
+print(Dict)
+
+Adding set of values
+to a single Key
+Dict['Value_set'] = 2, 3, 4
+print("\nDictionary after adding 3 elements: ")
+print(Dict)
+
+Updating existing Key's Value
+Dict[2] = 'hackoctberfest'
+print("\nUpdated key value: ")
+print(Dict)
+
+Adding Nested Key value to Dictionary
+Dict[5] = {'Nested': {'1': 'Life', '2': 'shivani'}}
+print("\nAdding a Nested Key: ")
+print(Dict)
