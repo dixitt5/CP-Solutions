caa8ff7dfc81699d6a7e050f05a73a281ff78b07
VIRAK-SAMA <71639960+VIRAK-SAMA@users.noreply.github.com>
2022-10-20 10:32:48 +0530

    Create GOLDEN RATIO (VA) NEW 22.java

---
+++ Java Programs/GOLDEN RATIO (VA) NEW 22.java
@@ -0,0 +1,17 @@
+// GOLDEN RATIO 
+class Main {
+  public static void main(String[] args) {
+
+    int n = 10, firstTerm = 0, secondTerm = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
+
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(firstTerm + ", ");
+
+      // Lets compute the next term
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+    }
+  }
+}
