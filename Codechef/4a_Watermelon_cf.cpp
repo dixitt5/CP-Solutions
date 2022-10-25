1c240c8274284f689cf574075ba9b1b99fc6af32
jayam30 <94307580+jayam30@users.noreply.github.com>
2022-10-25 18:17:41 +0530

    create waterprobelm.cpp

---
+++ Codechef/4a_Watermelon_cf.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ll w;
+    cin >> w;
+
+    if (w % 2 == 0 && w>2)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "no";
+    }
+
+
+    return 0;
+}
