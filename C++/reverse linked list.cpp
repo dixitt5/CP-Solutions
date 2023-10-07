e743e4f2980cf821b82ba2b9f1caf65f01ea4e68
D-Hacker26 <deepanshu.26.g@gmail.com>
2023-10-07 11:53:40 +0530

    reversed linked list

---
+++ C++/reverse linked list.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		next = NULL;
+	}
+};
+
+struct LinkedList
+{
+	Node *head;
+	LinkedList() { head = NULL; }
+
+	void reverse()
+	{
+
+		Node *current = head;
+		Node *prev = NULL, *next = NULL;
+
+		while (current != NULL)
+		{
+
+			next = current->next;
+
+			current->next = prev;
+
+			prev = current;
+			current = next;
+		}
+		head = prev;
+	}
+
+	void print()
+	{
+		struct Node *temp = head;
+		while (temp != NULL)
+		{
+			cout << temp->data << " ";
+			temp = temp->next;
+		}
+	}
+
+	void push(int data)
+	{
+		Node *temp = new Node(data);
+		temp->next = head;
+		head = temp;
+	}
+};
+
+int main()
+{
+
+	LinkedList ll;
+	ll.push(1);
+	ll.push(2);
+	ll.push(3);
+	ll.push(4);
+	ll.push(5);
+	ll.push(6);
+	ll.push(7);
+
+	cout << "Given linked list is\n";
+	ll.print();
+
+	ll.reverse();
+
+	cout << "\nReversed linked list \n";
+	ll.print();
+	return 0;
+}
