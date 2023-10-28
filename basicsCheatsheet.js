0c2bef97ebbf62beb142f47facfb18a4acb77dee
Dural Keys <duralinitaly@gmail.com>
2023-10-28 11:58:54 -0400

    Add files via upload

---
+++ basicsCheatsheet.js
@@ -0,0 +1,24 @@
+// Numbers:
+1;
+-99;
+0.345345;
+
+//Making variables with let:
+let numberOfFriends = 1;
+
+//Incrementing:
+numberOfFriends += 3; //numberOfFriends is now 4
+
+// Variables with const
+const minimumAge = 21; //CANNOT REASSIGN!
+
+//Booleans - true or false values
+true;
+false;
+let isHappy = true;
+
+//Naming Conventions
+// Use upper camel-cased names:
+let numberOfChickens = 6; //GOOD
+// NOT THE JS WAY:
+// let number_of_chickens = 6;
